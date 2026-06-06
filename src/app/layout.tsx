d990317de87bea7d921f070b0a4ee0358df280a8
Quentin <faber.quentin@gmail.com>
2026-06-06 22:20:24 +0200

    scroll

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -54,9 +54,7 @@ export default function RootLayout({
           <Header />
           <div className="flex flex-1">
             <Sidebar />
-            <main className="flex-1 xl:ml-72 overflow-y-auto relative">
-              {children}
-            </main>
+            <main className="flex-1 overflow-y-auto relative">{children}</main>
           </div>
           <Footer />
         </Providers>
